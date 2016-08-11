b9164bee26887a1252db53e55c56669958c9d5c0
Chris Sevilleja <chris@scotch.io>
2016-08-11 12:43:25 -0700

    10-robomongo

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -24,14 +24,17 @@ module.exports = {
     const events = [
       { name: 'Basketball', description: 'Throwing into a basket.' },
       { name: 'Swimming', description: 'Michael Phelps is the fast fish.' },
-      { name: 'Weightlifting', description: 'Lifting heavy things up' }
+      { name: 'Weightlifting', description: 'Lifting heavy things up' },
+      { name: 'Ping Pong', description: 'Super fast paddles' }
     ];
 
     // use the Event model to insert/save
-    for (event of events) {
-      var newEvent = new Event(event);
-      newEvent.save();
-    }
+    Event.remove({}, () => {
+      for (event of events) {
+        var newEvent = new Event(event);
+        newEvent.save();
+      }
+    });
 
     // seeded!
     res.send('Database seeded!');
