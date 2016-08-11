ecf4e832a4bc497059a95a48bfe9935af0396ee6
Chris Sevilleja <chris@scotch.io>
2016-08-10 20:04:50 -0700

    final

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,6 +1,8 @@
 module.exports = {
+
   // show the home page
   showHome: (req, res) => {
     res.render('pages/home');
   }
+  
 }
--- app/routes.js
+++ app/routes.js
@@ -10,11 +10,11 @@ module.exports = router;
 router.get('/', mainController.showHome);
 
 // events
-router.get('/events',            eventController.showAll);
-router.get('/events/seed',       eventController.seedEvents);
-router.get('/events/create',     eventController.showCreate);
-router.post('/events/create',    eventController.processCreate);
-router.get('/events/:slug',      eventController.showEvent);
-router.get('/events/:slug/edit', eventController.editEvent);
-router.post('/events/:slug',     eventController.processEdit);
-router.delete('/events/:id',     eventController.deleteEvent);
+router.get('/events',              eventController.showAll);
+router.get('/events/seed',         eventController.seedEvents);
+router.get('/events/create',       eventController.showCreate);
+router.post('/events/create',      eventController.processCreate);
+router.get('/events/:slug/edit',   eventController.editEvent);
+router.get('/events/:slug/delete', eventController.deleteEvent);
+router.get('/events/:slug',        eventController.showEvent);
+router.post('/events/:slug',       eventController.processEdit);
