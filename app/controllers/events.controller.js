608877522a214cb3f1a005605148edeec91c5b78
Chris Sevilleja <chris@scotch.io>
2016-08-11 13:00:25 -0700

    12-showing-real-events

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,43 +1,62 @@
 const Event = require('../models/event');
 
 module.exports = {
+  showEvents: showEvents,
+  showSingle: showSingle,
+  seedEvents: seedEvents
+}
 
-  // show all events
-  showEvents: (req, res) => {
-    // get all events   
+/**
+ * Show all events
+ */
+function showEvents(req, res) {
+  // get all events   
+  Event.find({}, (err, events) => {
+    if (err) {
+      res.status(404);
+      res.send('Events not found!');
+    }
 
     // return a view with data
     res.render('pages/events', { events: events });
-  },
+  });
+}
 
-  // show a single event
-  showSingle: (req, res) => {
-    // get a single event
-    const event = { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.' };
+/**
+ * Show a single event
+ */
+function showSingle(req, res) {
+  // get a single event
+  Event.findOne({ slug: req.params.slug }, (err, event) => {
+    if (err) {
+      res.status(404);
+      res.send('Event not found!');
+    }
 
     res.render('pages/single', { event: event });
-  },
+  });
+}
 
-  // seed our database
-  seedEvents: (req, res) => {
-    // create some events
-    const events = [
-      { name: 'Basketball', description: 'Throwing into a basket.' },
-      { name: 'Swimming', description: 'Michael Phelps is the fast fish.' },
-      { name: 'Weightlifting', description: 'Lifting heavy things up' },
-      { name: 'Ping Pong', description: 'Super fast paddles' }
-    ];
+/**
+ * Seed the database
+ */
+function seedEvents(req, res) {
+  // create some events
+  const events = [
+    { name: 'Basketball', description: 'Throwing into a basket.' },
+    { name: 'Swimming', description: 'Michael Phelps is the fast fish.' },
+    { name: 'Weightlifting', description: 'Lifting heavy things up' },
+    { name: 'Ping Pong', description: 'Super fast paddles' }
+  ];
 
-    // use the Event model to insert/save
-    Event.remove({}, () => {
-      for (event of events) {
-        var newEvent = new Event(event);
-        newEvent.save();
-      }
-    });
+  // use the Event model to insert/save
+  Event.remove({}, () => {
+    for (event of events) {
+      var newEvent = new Event(event);
+      newEvent.save();
+    }
+  });
 
-    // seeded!
-    res.send('Database seeded!');
-  }
-
-};
+  // seeded!
+  res.send('Database seeded!');
+}
