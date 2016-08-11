6930e6b6ae3574695d615fcd64ae0fbe56235236
Chris Sevilleja <chris@scotch.io>
2016-08-10 20:33:15 -0700

    01-starter

--- server.js
+++ server.js
@@ -1,34 +1,16 @@
-// load environment variables
-require('dotenv').config();
+// grab our dependencies
+const express = require('express'),
+  app = express(),
+  port = process.env.PORT || 8080;
 
-const express    = require('express'),
-  app            = express(),
-  morgan         = require('morgan'),
-  bodyParser     = require('body-parser'),
-  expressLayouts = require('express-ejs-layouts'),
-  mongoose       = require('mongoose');
+// configure our application
 
-// configure our app ================
-// grab info from forms
-app.use(bodyParser.urlencoded({ extended: true }));
+// set the routes
+app.get('/', (req, res) => {
+  res.send('Hello, I am the app!');
+});
 
-// log requests to the console
-app.use(morgan('dev'));
-
-// configure ejs and ejs layouts
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
-
-// set place for static assets
-app.use(express.static('public/'));
-
-// connect to our database
-mongoose.connect(process.env.DB_URL);
-
-// routes ===========================
-app.use(require('./app/routes'));
-
-// start the server =================
-app.listen(process.env.PORT, () => {
-  console.log(`App listening at http://localhost:${process.env.PORT}`);
+// start our server
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
 });
